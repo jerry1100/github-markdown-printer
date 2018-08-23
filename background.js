26687bbdd7dd167749b7339dc6246b91f6c22c2c
jerry1100 <jwu1100@gmail.com>
2018-08-23 01:07:15 -0700

    conditionally enable extension on page

--- background.js
+++ background.js
@@ -1,9 +1,21 @@
 /**
+ * Enables the page action when the conditions are met
+ */
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { hostEquals: 'github.com' },
+        css: ['article.markdown-body.entry-content'],
+      })],
+      actions: [new chrome.declarativeContent.ShowPageAction()],
+    }]);
+  });
+});
+
+/**
  * When the extension icon is clicked, send the current tab a message
  */
-chrome.browserAction.onClicked.addListener(tab => {
-  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-    const activeTab = tabs[0];
-    chrome.tabs.sendMessage(activeTab.id, { message: 'click' });
-  });
+chrome.pageAction.onClicked.addListener(tab => {
+  chrome.tabs.sendMessage(tab.id, { message: 'click' });
 });
