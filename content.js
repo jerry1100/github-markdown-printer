7a038415138ff43ad7df1280d4571e4cbb5c6779
jerry1100 <jwu1100@gmail.com>
2018-08-25 15:37:48 -0700

    set padding to 0 and border to none

--- content.js
+++ content.js
@@ -15,6 +15,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
           position: absolute;
           top: 0;
           left: 0;
+          padding: 0;
+          border: none;
         }
       }
     `;
