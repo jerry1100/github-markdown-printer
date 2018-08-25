afdef8b0dfc0c9ae8027026a54096ac24b792cb1
jerry1100 <jwu1100@gmail.com>
2018-08-25 16:20:05 -0700

    make everything else static

--- content.js
+++ content.js
@@ -6,17 +6,21 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     const style = document.createElement('style');
     style.innerHTML = `
       @media print {
-        body {
-          visibility: hidden;
+        body * {
+          visibility: hidden !important;
+          position: static !important;
         }
 
         article.markdown-body.entry-content {
-          visibility: visible !important;
-          position: absolute;
+          position: absolute !important;
           top: 0;
           left: 0;
           padding: 0;
           border: none;
+        }
+
+        article.markdown-body.entry-content * {
+          visibility: visible !important;
         }
       }
     `;
