389a8472d40b410b8e684f4c755169ff6283573b
jerry1100 <jwu1100@gmail.com>
2019-06-19 00:51:40 -0700

    add test script

---
+++ test/test.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+const { PNG } = require('pngjs');
+const pixelMatch = require('pixelmatch');
+const puppeteer = require('puppeteer');
+
+const TEST_SCREENSHOT_FILENAME = 'test-screenshot.png';
+const DIFF_SCREENSHOT_FILENAME = 'screenshot-diff.png';
+const referenceImage = PNG.sync.read(fs.readFileSync('./reference-screenshot.png'));
+
+(async () => {
+  await testURL('https://github.com/jerry1100/github-markdown-printer/tree/master/test');
+  await testURL('https://github.com/jerry1100/github-markdown-printer/blob/master/test/README.md');
+})();
+
+async function testURL(url) {
+  await takeScreenshot(url);
+
+  const numDiffPixels = await compareWithReference();
+  if (numDiffPixels !== 0) {
+    throw new Error(`Test and reference screenshots have ${numDiffPixels} different pixels`);
+  }
+
+  console.log(`[Passed]: Rendered markdown at ${url}`);
+}
+
+async function takeScreenshot(url) {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1200, height: 800 });
+  await page.goto(url);
+  await page.addStyleTag({ path: '../src/style.css' });
+  await page.emulateMedia('print');
+  await page.screenshot({ path: TEST_SCREENSHOT_FILENAME });
+  await browser.close();
+}
+
+async function compareWithReference() {
+  const testImage = PNG.sync.read(fs.readFileSync(TEST_SCREENSHOT_FILENAME));
+  const { width, height } = testImage;
+  const diff = new PNG({ width, height });
+  const numDiffPixels = pixelMatch(referenceImage.data, testImage.data, diff.data, width, height);
+  fs.writeFileSync(DIFF_SCREENSHOT_FILENAME, PNG.sync.write(diff));
+
+  return numDiffPixels;
+}
