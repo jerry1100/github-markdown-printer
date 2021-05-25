09e4425761599751575e665444e5e57897c30c01
jerry1100 <jwu1100@icloud.com>
2021-05-25 09:29:25 -1000

    set light theme before printing

--- src/content.js
+++ src/content.js
@@ -9,13 +9,19 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     link.addEventListener('load', () => {
       const bodyHtml = document.body.innerHTML;
       const markdownHtml = document.querySelector('.markdown-body').outerHTML;
+      const theme = document.documentElement.dataset.colorMode;
 
       // Have markdown content occupy entire page
       document.body.innerHTML = markdownHtml;
+
+      // Use light theme or else text contrast is bad
+      document.documentElement.dataset.colorMode = "light";
+
       window.print();
 
       // Clean up - revert to original
       document.body.innerHTML = bodyHtml;
+      document.documentElement.dataset.colorMode = theme;
       document.head.removeChild(link);
     });
   }
