7345ac8dba092cb51af8c340f82f1a2b012b4301
jerry1100 <jwu1100@icloud.com>
2021-12-03 21:54:44 -1000

    migrate to manifest v3

--- src/background.js
+++ src/background.js
@@ -1,17 +1,32 @@
-// Show the extension icon when certain conditions are met
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostContains: 'github' },
-        css: ['.markdown-body'],
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()],
-    }]);
-  });
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: printPage,
+    });
 });
 
-// When the extension is clicked, send the current tab a "click" message
-chrome.pageAction.onClicked.addListener(tab => {
-  chrome.tabs.sendMessage(tab.id, { message: 'click' });
-});
+function printPage() {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = chrome.runtime.getURL("style.css");
+
+    document.head.appendChild(link);
+    link.addEventListener("load", () => {
+        const bodyHtml = document.body.innerHTML;
+        const markdownHtml = document.querySelector(".markdown-body").outerHTML;
+        const theme = document.documentElement.dataset.colorMode;
+
+        // Have markdown content occupy entire page
+        document.body.innerHTML = markdownHtml;
+
+        // Use light theme or else text contrast is bad
+        document.documentElement.dataset.colorMode = "light";
+
+        window.print();
+
+        // Clean up - revert to original
+        document.body.innerHTML = bodyHtml;
+        document.documentElement.dataset.colorMode = theme;
+        document.head.removeChild(link);
+    });
+}
