dbf97b094c499aadf3e8c477717608affc03f826
Arpit Gupta <gigaarpit@gmail.com>
2024-07-26 14:10:51 +0530

    Create context menu entry only on GitHub.com pages
    Currently this extension is creating its context menu entry on all web pages, which is not intended if I'm not wrong. This PR uses `documentUrlPatterns` property to restrict context menu entry only on the github.com pages.
    
    Reference: https://developer.chrome.com/docs/extensions/reference/api/contextMenus#properties

--- src/background.js
+++ src/background.js
@@ -9,6 +9,7 @@ chrome.action.onClicked.addListener((tab) => {
 chrome.contextMenus.create({
   title: 'Print GitHub Markdown',
   id: GITHUB_MARKDOWN_PRINTER,
+  documentUrlPatterns: ["https://github.com/*"]
 });
 
 // Respond to context menu clicks
