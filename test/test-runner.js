e476765bfbdd2b4aa7954f3694917934df6c5652
jerry1100 <jwu1100@icloud.com>
2025-01-27 14:45:52 -0800

    disable sandbox

--- test/test-runner.js
+++ test/test-runner.js
@@ -31,7 +31,9 @@ class TestRunner {
   }
 
   async takeScreenshotAndSaveToFs() {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
 
     await page.setViewport({ width: 1200, height: 800 });
